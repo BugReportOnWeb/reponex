ee85a910ede3fe51bb64fb14714a552acdd1d9a5
Dev <flashytesting@gmail.com>
2024-01-09 01:20:19 +0530

    Fix network connectivity issues between server and db

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -1,13 +1,5 @@
 import { Pool } from 'pg';
 
-console.log({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST, 
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: Number(process.env.POSTGRES_PORT)
-})
-
 const db = new Pool({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST, 
