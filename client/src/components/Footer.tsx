e1e08bf96ad91c9fa5d07a9d4028c2d45f3ecf63
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-09 11:45:45 +0530

    Footer & Nav_Template Added

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import {
+  githubRepo,
+  copyrightYear
+} from "../utils/config";
+
+const Footer = () => {
+
+  const handleMouseEnter = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    e.currentTarget.style.color = "white";
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    e.currentTarget.style.color = "grey";
+  };
+
+  return (
+    <main className="w-full h-12 flex items-center justify-center font-heading">
+      <a href={githubRepo} target="_blank">
+        <FaGithub
+          style={{
+            fontSize: "26px",
+            color: "grey",
+            transition: "color 0.3s ease-in-out",
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        />
+      </a>
+      <span className="text-slate-400 mx-2">&copy; {copyrightYear} .</span>
+      <span className="text-slate-400">All rights reserved .</span>
+      <span className="text-slate-400 mx-2">Made with ❤️.</span>
+    </main>
+  );
+};
+
+export default Footer;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import NavBar from './components/NavBar.tsx'
+import Footer from './components/Footer.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <NavBar />
+    <App />
+    <Footer />
+  </React.StrictMode>,
 )
