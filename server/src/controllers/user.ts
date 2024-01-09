3a3a9e7162a0839a12255061b6239f36936e0918
Dev <flashytesting@gmail.com>
2024-01-09 21:23:34 +0530

    Add controller for fetching single user data

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { createTable } from "../db/queries";
 import db from "../db/pool";
+import { SafeUser } from "../types/user";
 
 // FOR DEBUGGING PURPOSE - REMOVE LATER
 // GET /api/users
@@ -14,6 +15,31 @@ const getAllUsers = async (_req: Request, res: Response) => {
 
         const users = result.rows;
         res.send(users);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).send({ error: error.message });
+        }
+    }
+}
+
+const getSingleUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        await createTable();
+        const result = await db.query(`
+            SELECT id, username
+            FROM users
+            WHERE id = $1
+        `, [id]);
+
+        if (result.rows.length < 1) {
+            const error = 'User doesn\'t exist';
+            return res.status(404).send({ error });
+        }
+
+        const user: SafeUser = result.rows[0];
+        res.send(user);
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).send({ error: error.message });
@@ -36,5 +62,6 @@ const truncateUsers = async (_req: Request, res: Response) => {
 
 export {
     getAllUsers,
+    getSingleUser,
     truncateUsers
 };
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllUsers,  truncateUsers } from "../controllers/user";
+import { getAllUsers,  getSingleUser,  truncateUsers } from "../controllers/user";
 import { loginUser, registerUser } from "../controllers/auth";
 
 const router = Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', getSingleUser);
 router.delete('/', truncateUsers);
 router.post('/login', loginUser);
 router.post('/register', registerUser);
