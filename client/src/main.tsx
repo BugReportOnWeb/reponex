8930b12d3b9ae9cd55067ca705a9ff707dc587cc
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-09 21:03:59 +0530

    Page Layout Update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import NavBar from './components/NavBar.tsx'
-import Footer from './components/Footer.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import NavBar from "./components/NavBar.tsx";
+import Footer from "./components/Footer.tsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register.tsx";
+import Login from "./pages/Login.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavBar />
-    <App />
-    <Footer />
+    <BrowserRouter>
+      <main className="max-w-5xl mx-auto min-h-screen">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </main>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
