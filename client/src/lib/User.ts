7beb502f03dc6cad1a142e63e72d8a8469714ec0
Dev <flashytesting@gmail.com>
2024-01-10 21:56:29 +0530

    Shift `GitHubUser` type under types dir

--- client/src/lib/User.ts
+++ client/src/lib/User.ts
@@ -1,52 +1,25 @@
-interface GitHubUser {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
-  name: string | null;
-  company: string | null;
-  blog: string;
-  location: string;
-  email: string | null;
-  hireable: boolean | null;
-  bio: string;
-  twitter_username: string | null;
-  public_repos: number;
-  public_gists: number;
-  followers: number;
-  following: number;
-  created_at: string;
-  updated_at: string;
+import { GitHubUser } from "../types/user";
+
+const getGitHubUser = async (username: string): Promise<GitHubUser> => {
+    try {
+        const url = `https://api.github.com/users/${username}`
+        const res = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(data.error);
+        }
+
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
-export const getUser: (user: string) => Promise<GitHubUser> = async (user: string) => {
-  try {
-    const url = `https://api.github.com/users/${user}`
-    const res = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await res.json();
-
-    return data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-}
+export { getGitHubUser };
