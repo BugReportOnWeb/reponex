3b8bdf69f40e0362429f95f6acde13ed20661b1f
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-11 23:39:24 +0530

    username fetched

--- client/src/context/AuthUserContext.tsx
+++ client/src/context/AuthUserContext.tsx
@@ -1,20 +1,45 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useState, useEffect } from "react";
 import { AuthUserContextType } from "../types/user";
 
 type AuthUserContextProviderProps = {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const AuthUserContext = createContext<AuthUserContextType | null>(null);
 
 const AuthUserContextProvider = ({ children }: AuthUserContextProviderProps) => {
-    const [authUser, setAuthUser] = useState('');
+  const [authUser, setAuthUser] = useState('');
 
-    return (
-        <AuthUserContext.Provider value={{ authUser, setAuthUser }}>
-            {children}
-        </AuthUserContext.Provider >
-    )
+  useEffect(() => {
+    // Fetch the username from the /api/users/me
+    const fetchUsername = async () => {
+      try {
+        const response = await fetch("/api/users/me", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setAuthUser(data.username);
+        } else {
+          setAuthUser("");
+        }
+      } catch (error) {
+        setAuthUser("");
+      }
+    };
+
+    fetchUsername();
+  }, []);
+
+  return (
+    <AuthUserContext.Provider value={{ authUser, setAuthUser }}>
+      {children}
+    </AuthUserContext.Provider >
+  )
 }
 
 export { AuthUserContextProvider, AuthUserContext };
