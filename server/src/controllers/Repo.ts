52f2b81ff7f45c3891df041cb00a394d4f72b620
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-13 19:53:50 +0530

    Update Repo API Added

--- server/src/controllers/Repo.ts
+++ server/src/controllers/Repo.ts
@@ -40,6 +40,46 @@ const createRepo = async (req: Request, res: Response) => {
   }
 }
 
+// PATCH /api/repos/update/:owner/:repo 
+const updateRepo = async (req: Request, res: Response) => {
+  const { owner, repo } = req.params;
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    const error = 'Auth token not provided';
+    return res.status(400).send({ error });
+  }
+
+  const token = authorization.split(' ')[1];
+
+  try {
+    const url = `https://api.github.com/repos/${owner}/${repo}`;
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: req.body.name,
+        description: req.body.description,
+        private: req.body.private,
+      }),
+    });
+
+    if (response.ok) {
+      const updatedRepo = await response.json();
+      res.json(updatedRepo);
+    } else {
+      const errorResponse = await response.json();
+      res.status(response.status).json({ error: errorResponse });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
 // DELETE /api/repos/delete/:owner/:repo
 const deleteRepo = async (req: Request, res: Response) => {
   const { owner, repo } = req.params;
@@ -76,4 +116,5 @@ const deleteRepo = async (req: Request, res: Response) => {
 export {
   deleteRepo,
   createRepo,
+  updateRepo,
 }
--- server/src/routes/repo.ts
+++ server/src/routes/repo.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import {
   deleteRepo,
   createRepo,
+  updateRepo
 } from "../controllers/Repo";
 
 const router = Router();
 
 // Delete Repo 
 router.delete('/delete/:owner/:repo', deleteRepo);
-// Create REpo
+// Create Repo
 router.post('/create', createRepo);
+// Update Repo 
+router.patch('/update/:owner/:repo', updateRepo);
 
 export { router as repoRoutes };
