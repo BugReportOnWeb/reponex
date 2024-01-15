401dc34347dbfc3d8c3539e553b8142945ae7ff4
Dev <flashytesting@gmail.com>
2024-01-15 19:26:46 +0530

    Add handler for creating a new issue

--- client/src/components/actions/IssueAction.tsx
+++
@@ -1,9 +0,0 @@
-const IssueAction = () => {
-    return (
-        <div className=''>
-            Issue Action
-        </div>
-    )
-}
-
-export default IssueAction;
--- client/src/components/actions/issue/CreateIssueForm.tsx
+++ client/src/components/actions/issue/CreateIssueForm.tsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
 
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+
 const CreateIssueForm = () => {
     const [repoName, setRepoName] = useState('');
     const [repoOwner, setRepoOwner] = useState('');
     const [issueTitle, setIssueTitle] = useState('');
     const [issueBody, setIssueBody] = useState('');
 
-    const createIssue = () => {
-        console.log({ action: 'CREATE', repoName, repoOwner, issueTitle, issueBody });
+    const createIssue = async () => {
+        const issueDetails = {
+            title: issueTitle,
+            body: issueBody
+        }
+
+        console.log({ action: 'CREATE', repoName, repoOwner, ...issueDetails });
+
+        // TODO: Move to lib
+        try {
+            const res = await fetch(`http://localhost:3000/api/repos/issues/create/${repoOwner}/${repoName}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${GITHUB_TOKEN}`
+                },
+                body: JSON.stringify(issueDetails)
+            })
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                throw new Error(data.error);
+            }
+
+            // TODO: Handle success and error accordingly
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
 
         setRepoName('');
         setRepoOwner('');
@@ -60,7 +90,6 @@ const CreateIssueForm = () => {
                     className='h-40 bg-transparent border border-[#272731] px-3.5 py-2.5 text-sm rounded-lg placeholder-[#A1A1AA] outline-none'
                     required
                 />
-
                 <button onClick={createIssue} type="button" className='mt-3 inline-flex w-full whitespace-nowrap items-center justify-center px-4 py-2 border border-[#27272a] font-medium text-sm rounded-md transition-colors cursor-pointer bg-white text-[#27272a] hover:bg-white/90'>
                     Create Issue
                 </button>
