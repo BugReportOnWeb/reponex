931005bbb5dfe56fdb17eed013e00d7294413371
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-15 17:42:43 +0530

    User's Data, Repo's Data, Event's Data added in DashBoard

--- client/src/lib/User.ts
+++ client/src/lib/User.ts
@@ -1,25 +1,90 @@
 import { GitHubUser } from "../types/user";
 
 const getGitHubUser = async (username: string): Promise<GitHubUser> => {
-    try {
-        const url = `https://api.github.com/users/${username}`
-        const res = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        const data = await res.json();
+  const userData = localStorage.getItem("userData");
 
-        if (!res.ok) {
-            throw new Error(data.error);
-        }
-
-        return data;
-    } catch (error) {
-        console.error(error);
-        throw error;
+  try {
+    if (userData) {
+      return JSON.parse(userData);
     }
-}
+    const url = `https://api.github.com/users/${username}`;
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
 
-export { getGitHubUser };
+    localStorage.setItem("userData", data);
+
+    if (!res.ok) {
+      throw new Error(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getGitHubUserRepos = async (username: string) => {
+  const reposData = localStorage.getItem("reposData");
+
+  try {
+    if (reposData) {
+      return JSON.parse(reposData);
+    }
+
+    const url = `https://api.github.com/users/${username}/repos`;
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    localStorage.setItem("reposData", data);
+
+    if (!res.ok) {
+      throw new Error(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const getGitHubUserEvents = async (username: string) => {
+  const eventsData = localStorage.getItem("eventsData");
+  try {
+    if (eventsData) {
+      return JSON.parse(eventsData);
+    }
+
+    const url = `https://api.github.com/users/${username}/events`;
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    localStorage.setItem("eventsData", data);
+
+    if (!res.ok) {
+      throw new Error(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+export { getGitHubUser, getGitHubUserRepos, getGitHubUserEvents };
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,20 +1,48 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { AuthUserContext } from "../context/AuthUserContext";
 import { AuthUserContextType } from "../types/user";
-import { userData } from "../localStore/userData";
 import UserData from "../components/UserData";
-import { eventData } from "../localStore/eventData";
-import { repoData } from "../localStore/repoData";
 import RepoCard from "../components/actions/repository/RepoCard";
 import { Repository } from "../types/repo";
 import ActivityCard from "../components/actions/repository/ActivityCard";
+import {
+  getGitHubUser,
+  getGitHubUserRepos,
+  getGitHubUserEvents,
+} from "../lib/User";
 
 const Dashboard = () => {
   const { authUser } = useContext(AuthUserContext) as AuthUserContextType;
-  const IssuesData = eventData.filter(item => item.type === "IssuesEvent");
-  const PullRequestData = eventData.filter(item => item.type === "PullRequestEvent");
 
-  const repositories = repoData.map(repo => {
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+  const [repoData, setRepoData] = useState([]);
+  const [eventData, setEventData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Set loading to true when starting the fetch
+        setLoading(true);
+
+        const userDataFetch = await getGitHubUser(authUser);
+        const repoDataFetch = await getGitHubUserRepos(authUser);
+        const eventDataFetch = await getGitHubUserEvents(authUser);
+        setUserData(userDataFetch);
+        setRepoData(repoDataFetch);
+        setEventData(eventDataFetch);
+      } catch (error: any) {
+        console.error("Error fetching board list:", error.message);
+      } finally {
+        // Set loading to false when fetch is completed (either success or error)
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [authUser]);
+
+  const repositories = repoData.map((repo) => {
     const repoDetails: Repository = {
       name: repo.name,
       url: repo.html_url,
@@ -23,66 +51,89 @@ const Dashboard = () => {
       watchers_count: repo.watchers_count,
       stargazers_count: repo.stargazers_count,
       forks_count: repo.forks_count,
-      open_issues_count: repo.open_issues_count
-    }
+      open_issues_count: repo.open_issues_count,
+    };
     return repoDetails;
-  })
+  });
+
+  const IssuesData = eventData.filter((item) => item.type === "IssuesEvent");
+  const PullRequestData = eventData.filter(
+    (item) => item.type === "PullRequestEvent",
+  );
 
   return (
-    <main className='p-5 pb-20 w-full'>
-      <h1 className="text-3xl text-white font-semibold mb-7">Hello, {userData.login}!</h1>
-      <section className="w-full h-[calc(100vh-15rem)] grid grid-cols-4 grid-rows-2 gap-x-4 gap-y-3">
-        <div className="flex col-span-2 border border-gray-700 hover:border-[#ededed]/60 rounded-lg w-full h-full">
-          <div className="w-1/3 h-full p-3 flex flex-col gap-3 justify-center items-center00">
-            <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
-              <img src={userData.avatar_url} alt={userData.name} className="rounded-full transition-all duration-300 ease-in-out hover:border hover:border-cyan-500" />
-            </a>
+    <main className="p-5 pb-20 w-full">
+      <h1 className="text-3xl text-white font-semibold mb-7">
+        Hello, {authUser}!
+      </h1>
+      {!loading && (
+        <section className="w-full h-[calc(100vh-15rem)] grid grid-cols-4 grid-rows-2 gap-x-4 gap-y-3">
+          <div className="flex col-span-2 border border-gray-700 hover:border-[#ededed]/60 rounded-lg w-full h-full">
+            <div className="w-1/3 h-full p-3 flex flex-col gap-3 justify-center items-center00">
+              <a
+                href={userData.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={userData.avatar_url}
+                  alt={userData.name}
+                  className="rounded-full transition-all duration-300 ease-in-out hover:border hover:border-cyan-500"
+                />
+              </a>
+            </div>
+            <div className="w-2/3">
+              <UserData userData={userData} />
+            </div>
           </div>
-          <div className="w-2/3">
-            <UserData userData={userData} />
+          <div className="col-span-2 flex flex-col gap-1.5 p-2">
+            <h1 className="font-extrabold text-2xl mb-2">Repositories List</h1>
+            <div className="overflow-y-auto">
+              {repositories.map((repo) => (
+                <RepoCard repo={repo} />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className='col-span-2 flex flex-col gap-1.5 p-2'>
-          <h1 className='font-extrabold text-2xl mb-2'>Repositories List</h1>
-          <div className="overflow-y-auto">
-            {repositories.map(repo => (
-              <RepoCard repo={repo} />
-            ))}
+          <div className="flex flex-col gap-2 p-2">
+            <h1 className="font-extrabold text-xl">Pull Requests</h1>
+            <div className="overflow-y-auto">
+              {PullRequestData.map((issue, index) => (
+                <div
+                  key={issue.id}
+                  className="rounded-md border-gray-700 p-2 text-sm flex "
+                >
+                  <span className="mr-2">{index + 1}</span>
+                  <h1>{issue.payload.pull_request?.title}</h1>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-2 p-2">
-          <h1 className='font-extrabold text-xl'>Pull Requests</h1>
-          <div className='overflow-y-auto'>
-            {PullRequestData.map((issue, index) => (
-              <div key={issue.id} className="rounded-md border-gray-700 p-2 text-sm flex ">
-                <span className='mr-2'>{index + 1}</span>
-                <h1>{issue.payload.pull_request?.title}</h1>
-              </div>
-            ))}
+          <div className="flex flex-col gap-2 p-2 border border-gray-700 hover:border-[#ededed]/60">
+            <h1 className="font-bold text-xl">Issues</h1>
+            <div className="overflow-y-auto">
+              {IssuesData.map((issue) => (
+                <div
+                  key={issue.id}
+                  className="border rounded-md border-gray-700 p-2 text-sm"
+                >
+                  {issue.payload.issue?.title}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-2 p-2 border border-gray-700 hover:border-[#ededed]/60">
-          <h1 className='font-bold text-xl'>Issues</h1>
-          <div className='overflow-y-auto'>
-            {IssuesData.map(issue => (
-              <div key={issue.id} className="border rounded-md border-gray-700 p-2 text-sm">
-                {issue.payload.issue?.title}
-              </div>
-            ))}
+          <div className="col-span-2 border border-gray-700 flex flex-col gap-2 p-2 hover:border-[#ededed]/60 rounded-lg">
+            <h1 className="font-extrabold text-2xl">Activity Feeds</h1>
+            <div className="overflow-y-auto">
+              {eventData.map((event) => (
+                // @ts-expect-error improve types
+                <ActivityCard event={event} />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="col-span-2 border border-gray-700 flex flex-col gap-2 p-2 hover:border-[#ededed]/60 rounded-lg">
-          <h1 className='font-extrabold text-2xl'>Activity Feeds</h1>
-          <div className="overflow-y-auto">
-            {eventData.map(event => (
-              // @ts-expect-error improve types
-              <ActivityCard event={event} />
-            ))}
-          </div>
-        </div>
-      </section>
+        </section>
+      )}
     </main>
   );
-}
+};
 
 export default Dashboard;
