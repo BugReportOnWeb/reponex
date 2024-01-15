8e6d1ceab9bbe34e78ed4526168db03682072a39
Dev <flashytesting@gmail.com>
2024-01-16 00:35:25 +0530

    Minor update rules page UI

--- client/src/pages/Rules.tsx
+++ client/src/pages/Rules.tsx
@@ -2,8 +2,8 @@ import { getRules } from "../lib/config";
 
 const Rules = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold text-center mb-3">General Rules</h1>
+        <div className='mt-5'>
+            <h1 className="text-3xl font-bold text-center mb-5">General Rules</h1>
             <div className="w-full h-full flex flex-col items-center pb-20">
                 {getRules.map((rule, index) => (
                     <div key={index} className="w-[95%] flex flex-col border border-gray-700 rounded-xl px-3.5 py-4 m-2.5">
