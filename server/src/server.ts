e1ac19307285bed4893de39c8934e49572a387e9
Dev <flashytesting@gmail.com>
2024-01-15 12:43:21 +0530

    Add socket client origin for docker on 8000 port

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: `http://localhost:${CLIENT_PORT}`
+        origin: [
+            `http://localhost:${CLIENT_PORT}`,
+            'http://localhost:8000'
+        ]
     }
 });
 
