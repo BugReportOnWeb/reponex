b02704b1a5f53e9a2e9cf88447194594d7eef85e
falcon71181 <dertyabbuiloveyou71@gmail.com>
2024-01-15 14:05:40 +0530

    ActivityEvents Card

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -7,6 +7,7 @@ import { eventData } from "../localStore/eventData";
 import { repoData } from "../localStore/repoData";
 import RepoCard from "../components/actions/repository/RepoCard";
 import { Repository } from "../types/repo";
+import ActivityCard from "../components/actions/repository/ActivityCard";
 
 const Dashboard = () => {
   const { authUser } = useContext(AuthUserContext) as AuthUserContextType;
@@ -74,67 +75,8 @@ const Dashboard = () => {
           <h1 className='font-extrabold text-2xl'>Activity Feeds</h1>
           <div className="overflow-y-auto">
             {eventData.map(event => (
-              <div key={event.id} className="flex items-center border rounded-md border-gray-700 gap-2 p-2 text-sm">
-                <div className="w-1/6 font-medium text-base">{event.type.split("E")[0]}</div>
-                {event.type === "IssuesEvent" && (
-                  <a href={event.payload.issue?.html_url} className="w-5/6 flex justify-between text-sm text-[#ededed]/60 hover:text-blue-500">
-                    <h1>
-                      {event.payload.issue?.title}
-                    </h1>
-                    <h1 className={event.payload.issue?.state === "open" ? "text-red-500 font-semibold uppercase" : "text-green-500 font-semibold uppercase"}>
-                      {event.payload.issue?.state}
-                    </h1>
-                  </a>
-                )}
-                {event.type === "PullRequestEvent" && (
-                  <a href={event.payload.pull_request?.html_url} className="w-5/6 flex gap-1 justify-between text-sm text-[#ededed]/60 hover:text-blue-500">
-                    <h1>
-                      {event.payload.pull_request?.title}
-                    </h1>
-                    <h1 className={event.payload.pull_request?.state === "open" ? "text-red-500 font-semibold uppercase" : "text-green-500 font-semibold uppercase"}>
-                      {event.payload.pull_request?.state}
-                    </h1>
-                  </a>
-                )}
-                {event.type === "WatchEvent" && (
-                  <div className="w-5/6 flex gap-1 text-[#ededed]/60">
-                    <h1>{event.actor.login}</h1>
-                    <h3 className="font-semibold">{event.payload.action}</h3>
-                    <h1>{event.repo.name}</h1>
-                  </div>
-                )}
-                {event.type === "ForkEvent" && (
-                  <a href={event.payload.forkee?.html_url} className="w-5/6 flex justify-between text-sm text-[#ededed]/60 hover:text-blue-500">
-                    <div className="flex gap-1">
-                      <h1>{event.actor.login}</h1>
-                      <h3 className="font-semibold">forked</h3>
-                      <h1>{event.repo.name}</h1>
-                    </div>
-                    {/* Added fork icon */}
-                  </a>
-                )}
-                {event.type === "PushEvent" && (
-                  <div className="w-5/6 flex gap-1 text-[13px] text-[#ededed]/60">
-                    <h1>{event.actor.login} pushed</h1>
-                    <h1 className="font-semibold">{event.payload.commits?.length} commits</h1>
-                    <h1>to {event.repo.name}</h1>
-                  </div>
-                )}
-                {event.type === "CreateEvent" && (
-                  <a href={`https://github.com/${event.repo.name}/tree/${event.payload.master_branch}`} className="w-5/6 text-sm flex gap-1 text-[#ededed]/60 hover:text-blue-500">
-                    <h1>Created </h1>
-                    <h3 className="font-semibold uppercase">{event.payload.ref_type}</h3>
-                    <h1>- {event.repo.name} #{event.payload.master_branch}</h1>
-                  </a>
-                )}
-                {event.type === "DeleteEvent" && (
-                  <div className="w-5/6 text-sm flex gap-1 text-[#ededed]/60">
-                    <h1>Deleted </h1>
-                    <h3 className="font-semibold uppercase">{event.payload.ref_type}</h3>
-                    <h1>- {event.repo.name} #{event.payload.ref}</h1>
-                  </div>
-                )}
-              </div>
+              // @ts-expect-error improve types
+              <ActivityCard event={event} />
             ))}
           </div>
         </div>
