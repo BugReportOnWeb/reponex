77f0be195afeddd1190008a71bc74c2b25a07d32
Dev <flashytesting@gmail.com>
2024-01-16 16:18:20 +0530

    Clear all localStorage items on logout

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -21,68 +21,65 @@ import NavLink from "./NavLink";
 import socket from "../socket/socket";
 
 const NavBar = () => {
-  const { authUser, setAuthUser } = useContext(
-    AuthUserContext,
-  ) as AuthUserContextType;
-  const { setMessageDataLogs } = useContext(
-    MessageLogsContext,
-  ) as MessageLogsContextType;
+    const { authUser, setAuthUser } = useContext(
+        AuthUserContext,
+    ) as AuthUserContextType;
+    const { setMessageDataLogs } = useContext(
+        MessageLogsContext,
+    ) as MessageLogsContextType;
 
-  const logoutUser = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userData");
-    localStorage.removeItem("reposData");
-    localStorage.removeItem("eventsData");
-    socket.disconnect();
+    const logoutUser = () => {
+        localStorage.clear();
+        socket.disconnect();
 
-    setAuthUser("");
-    setMessageDataLogs([]);
-  };
+        setAuthUser("");
+        setMessageDataLogs([]);
+    };
 
-  return (
-    <div className="p-5 w-full flex items-center justify-between">
-      <Link to="/" className="font-bold text-2xl text-[#ededed] select-none">
-        RepoNex
-      </Link>
-      <div className="h-full flex gap-6">
-        {authUser ? (
-          <>
-            <NavLink href="/chat">
-              <BsGlobe2 style={{ fontSize: "15px" }} />
-              <h1 className="text-[0.9375rem]">Chat</h1>
-            </NavLink>
-            <NavLink href="/actions">
-              <IoCodeWorking style={{ fontSize: "15px" }} />
-              <h1 className="text-[0.9375rem]">Actions</h1>
-            </NavLink>
-            <button
-              onClick={logoutUser}
-              className="flex items-center gap-2 text-[#ededed]/60 transition-colors ease-in-out hover:text-[#ededed]/80"
-            >
-              <CiLogout style={{ fontSize: "15px" }} />
-              <h1 className="text-[0.9375rem]">Logout</h1>
-            </button>
-          </>
-        ) : (
-          <>
-            <NavLink href="/login">
-              <FiLogIn style={{ fontSize: "15px" }} />
-              <h1 className="text-[0.9375rem]">Login</h1>
-            </NavLink>
-            <NavLink href="/register">
-              <IoPersonAddSharp style={{ fontSize: "15px" }} />
-              <span className="text-[0.9375rem]">Register</span>
-            </NavLink>
-          </>
-        )}
-        <div className="border border-white/40"></div>
-        <NavLink href="/rules">
-          <IoBookOutline style={{ fontSize: "15px" }} />
-          <h1 className="text-[0.9375rem]">Rules</h1>
-        </NavLink>
-      </div>
-    </div>
-  );
+    return (
+        <div className="p-5 w-full flex items-center justify-between">
+            <Link to="/" className="font-bold text-2xl text-[#ededed] select-none">
+                RepoNex
+            </Link>
+            <div className="h-full flex gap-6">
+                {authUser ? (
+                    <>
+                        <NavLink href="/chat">
+                            <BsGlobe2 style={{ fontSize: "15px" }} />
+                            <h1 className="text-[0.9375rem]">Chat</h1>
+                        </NavLink>
+                        <NavLink href="/actions">
+                            <IoCodeWorking style={{ fontSize: "15px" }} />
+                            <h1 className="text-[0.9375rem]">Actions</h1>
+                        </NavLink>
+                        <button
+                            onClick={logoutUser}
+                            className="flex items-center gap-2 text-[#ededed]/60 transition-colors ease-in-out hover:text-[#ededed]/80"
+                        >
+                            <CiLogout style={{ fontSize: "15px" }} />
+                            <h1 className="text-[0.9375rem]">Logout</h1>
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <NavLink href="/login">
+                            <FiLogIn style={{ fontSize: "15px" }} />
+                            <h1 className="text-[0.9375rem]">Login</h1>
+                        </NavLink>
+                        <NavLink href="/register">
+                            <IoPersonAddSharp style={{ fontSize: "15px" }} />
+                            <span className="text-[0.9375rem]">Register</span>
+                        </NavLink>
+                    </>
+                )}
+                <div className="border border-white/40"></div>
+                <NavLink href="/rules">
+                    <IoBookOutline style={{ fontSize: "15px" }} />
+                    <h1 className="text-[0.9375rem]">Rules</h1>
+                </NavLink>
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
